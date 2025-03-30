eb9c67dc9cc7a5749c77b82c589dfe34ceea36f4
RobertoCC05 <camposcalvo1@gmail.com>
2025-03-29 23:42:57 -0600

    useMemo Example

--- hooks/useMemo-hook/src/App.jsx
+++ hooks/useMemo-hook/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { useState, useMemo } from 'react';
+
+export default function App() {
+  const [users, setUsers] = useState(['Ana', 'Carlos', 'Elena', 'Beatriz', 'David']);
+  const [filter, setFilter] = useState('');
+
+  // useMemo: evita recalcular filteredUsers si [users, filter] no cambian
+  const filteredUsers = useMemo(() => {
+    console.log('Recalculando filtro...'); // verifica que solo se ejecuta cuando cambia el filtro
+    return users.filter(user => 
+      user.toLowerCase().includes(filter.toLowerCase())
+    );
+  }, [users, filter]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div style={{ padding: '20px' }}>
+      <h2>Lista filtrada con useMemo</h2>
+      <input
+        type="text"
+        value={filter}
+        //Actualiza cada vez que hay un cambio en el input
+        onChange={(e) => setFilter(e.target.value)}
+        placeholder="Filtrar nombres..."
+      />
+      <ul>
+        
+        {filteredUsers.map(user => (
+          <li key={user}>{user}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
-
-export default App
