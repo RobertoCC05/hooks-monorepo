1562e01fdb7d3859da70f9820f62b1fa6f3cc9d9
RobertoCC05 <camposcalvo1@gmail.com>
2025-03-29 22:35:39 -0600

    useContext example

--- hooks/useContext-hook/src/App.jsx
+++ hooks/useContext-hook/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { createContext, useContext, useState} from 'react';
 
+// crear contexto (valor por defecto 'light')
+const TemaContext = createContext('light');
+
+// El componente que PROVEE el contexto (padre)
+function App() {
+  const [tema, setTema] = useState('light'); // use state para modificar el estado del contexto
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    // Acá se modifica el valor actual de "tema" para todo el contexto
+    <TemaContext.Provider value={tema}>
+      <div style={{ background: tema === 'light' ? '#fff' : '#333', padding: '20px' }}>
+        <MostrarTema />
+        <button onClick={() => setTema(tema === 'light' ? 'dark' : 'light')}>
+          Cambiar tema
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </TemaContext.Provider>
+  );
+}
+
+// el componente que consume el contexto (hijo)
+function MostrarTema() {
+  const tema = useContext(TemaContext); // Lee el valor del contexto
+  return <p style={{color: 'grey'}}>El tema actual es: {tema}</p>;
 }
 
 export default App
